4281e805e7b766ec5c39e97d71bc778ad2c0ebe1
Antoine Boucher <antoine.bou13@gmail.com>
2024-06-26 12:11:34 -0400

    Update models.py

--- simulateur/simulation/models.py
+++ simulateur/simulation/models.py
@@ -5,13 +5,6 @@ from django.contrib.auth.models import User
 class Company(models.Model):
     name = models.CharField(max_length=100, default='')
     backstory = models.TextField(default='')
-    max_shares = models.IntegerField(default=1000)
-    price_maximum = models.FloatField(default=0.0)
-    initial_price = models.FloatField(default=0.0)
-    current_price = models.FloatField(default=0.0)
-    last_updated = models.DateTimeField(auto_now=True)
-    pe_ratio = models.FloatField(default=0.0)
-    market_cap = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
@@ -21,17 +14,11 @@ class Company(models.Model):
 
 class Stock(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    ticker = models.CharField(max_length=10, default='')
     price = models.FloatField(default=0.0)
     last_updated = models.DateTimeField(auto_now=True)
     partial_share = models.FloatField(default=0.0)
     complete_share = models.IntegerField(default=0)
-    open_price = models.FloatField(default=0.0)
-    high_price = models.FloatField(default=0.0)
-    low_price = models.FloatField(default=0.0)
-    close_price = models.FloatField(default=0.0)
-    quantity = models.IntegerField(default=0)
-    value_change = models.FloatField(default=0.0)
-    value_history = models.JSONField(default=list)
 
     def __str__(self):
         return f'{self.company.name} Stock'
@@ -39,31 +26,9 @@ class Stock(models.Model):
     class Meta:
         verbose_name_plural = "Stocks"
 
-class Cryptocurrency(models.Model):
-    name = models.CharField(max_length=100, default='')
-    price = models.FloatField(default=0.0)
-    last_updated = models.DateTimeField(auto_now=True)
-    price_maximum = models.FloatField(default=0.0)
-    partial_share = models.FloatField(default=0.0)
-    complete_share = models.IntegerField(default=0)
-    open_price = models.FloatField(default=0.0)
-    high_price = models.FloatField(default=0.0)
-    low_price = models.FloatField(default=0.0)
-    close_price = models.FloatField(default=0.0)
-    quantity = models.FloatField(default=0.0)
-    value_change = models.FloatField(default=0.0)
-    value_history = models.JSONField(default=list)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "Cryptocurrencies"
-
 class Team(models.Model):
     name = models.CharField(max_length=100, default='')
     balance = models.FloatField(default=100000)
-    borrowed_money = models.FloatField(default=0.0)
     members = models.ManyToManyField('UserProfile', related_name='teams')
 
     def __str__(self):
@@ -76,7 +41,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.FloatField(default=10000)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True, related_name='user_profiles')
-    borrowed_money = models.FloatField(default=0.0)
 
     def __str__(self):
         return self.user.username
@@ -102,22 +66,13 @@ class Trigger(models.Model):
     description = models.TextField(default='')
     trigger_type = models.CharField(max_length=100, default='')
     trigger_value = models.FloatField(default=0.0)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = "Triggers"
-
-class CustomStat(models.Model):
-    name = models.CharField(max_length=100, default='')
-    value = models.FloatField(default=0.0)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "Custom Stats"
 
 class SimulationSettings(models.Model):
     TIME_UNIT_CHOICES = [
@@ -171,28 +126,14 @@ class Scenario(models.Model):
     duration = models.IntegerField(default=0)  # Duration in seconds
     companies = models.ManyToManyField(Company)
     stocks = models.ManyToManyField(Stock)
-    cryptocurrencies = models.ManyToManyField(Cryptocurrency)
     users = models.ManyToManyField(UserProfile)
     teams = models.ManyToManyField(Team)
     events = models.ManyToManyField(Event)
     triggers = models.ManyToManyField(Trigger)
-    custom_stats = models.ManyToManyField(CustomStat)
     simulation_settings = models.OneToOneField(SimulationSettings, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
-    def get_companies(self):
-        return ', '.join([company.name for company in self.companies.all()])
-
-    def get_stocks(self):
-        return ', '.join([stock.company.name for stock in self.stocks.all()])
-
-    def get_users(self):
-        return ', '.join([user.user.username for user in self.users.all()])
-
-    def get_teams(self):
-        return ', '.join([team.name for team in self.teams.all()])
 
     class Meta:
         verbose_name_plural = "Scenarios"
@@ -201,16 +142,9 @@ class Portfolio(models.Model):
     owner = models.OneToOneField(UserProfile, on_delete=models.CASCADE, null=True, blank=True, related_name='portfolio')
     team = models.OneToOneField(Team, on_delete=models.CASCADE, null=True, blank=True, related_name='portfolio')
     stocks = models.ManyToManyField(Stock)
-    cryptocurrencies = models.ManyToManyField(Cryptocurrency)
 
     def __str__(self):
         return f'Portfolio for {self.owner or self.team}'
-
-    def get_stocks(self):
-        return ', '.join([stock.company.name for stock in self.stocks.all()])
-
-    def get_cryptocurrencies(self):
-        return ', '.join([crypto.name for crypto in self.cryptocurrencies.all()])
 
     class Meta:
         verbose_name_plural = "Portfolios"
@@ -254,25 +188,8 @@ class SimulationData(models.Model):
         else:
             return timedelta(seconds=0)
 
-    def display_price_changes(self):
-        return self.price_changes
-
-    display_price_changes.short_description = 'Price Changes'
-
     class Meta:
         verbose_name_plural = "Simulation Data"
-
-class Exchange(models.Model):
-    name = models.CharField(max_length=100, default='')
-    description = models.TextField(default='')
-    stocks = models.ManyToManyField(Stock)
-    cryptocurrencies = models.ManyToManyField(Cryptocurrency)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = "Exchanges"
 
 class Order(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
@@ -293,23 +210,10 @@ class News(models.Model):
     content = models.TextField(default='')
     published_date = models.DateTimeField(auto_now=True)
     impact = models.FloatField(default=0.0)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
         verbose_name_plural = "Market News"
-
-class Transaction(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    ticker = models.CharField(max_length=100, default='')
-    quantity = models.IntegerField(default=0)
-    price = models.FloatField(default=0.0)
-    transaction_type = models.CharField(max_length=10, default='buy')
-    timestamp = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'Transaction for {self.ticker}'
-
-    class Meta:
-        verbose_name_plural = "Transactions"
